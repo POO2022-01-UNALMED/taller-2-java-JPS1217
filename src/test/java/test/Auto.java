bb1ba4e7e4a7e1f8018c83d7e6b80fcfe47e9174
JPS1217 <jspatinoa@unal.edu.co>
2022-03-22 23:26:07 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,38 +1,35 @@
 package test;
 public class Auto {
-    String modelo;
-    String marca;
-    Motor motor;
-    int precio;
-    int registro;
-    static int cantidadCreados;
-    Asiento Asientos[];
+        String modelo;
+        String marca;
+        Motor motor;
+        int precio;
+        int registro;
+        static int cantidadCreados;
+        Asiento Asientos[];
 
-    void verificarintegridad() {
-        int control = this.registro;
-        int control2 = 0;
-        if(control == this.motor.registro){
+        void verificarintegridad() {
+            int control = this.registro;
+            int control2 = 0;
+            if(control == this.motor.registro){
+                for (int i = 0; i < this.Asientos.length; i++) {
+                    if(control != this.Asientos[i].registro) {
+                        control2++;
+                    }
+                }
+                if (control2 != 0) {
+                    System.out.print("Las piezas no son originales");
+                }else System.out.print("Auto original");
+            }else System.out.print("Las piezas no son originales");
+        }
+
+        int cantidadAsientos(){
+            int contador = 0;
             for (int i = 0; i < this.Asientos.length; i++) {
-                if(control != this.Asientos[i].registro) {
-                    control2++;
+                if (this.Asientos[i] instanceof Asiento){
+                    contador++;
                 }
             }
-            if (control2 != 0) {
-                System.out.print("Las piezas no son originales");
-            }else System.out.print("Auto original");
-        }else System.out.print("Las piezas no son originales");
-    }
-
-    int cantidadAsientos(){
-        int contador = 0;
-        for (int i = 0; i < this.Asientos.length; i++) {
-            if (this.Asientos[i] instanceof Asiento){
-                contador++;
-            }
+            return contador;
         }
-        return contador;
-    }
-    public static void main(String args[]) {
-
-    }
 }
